1f47797bd7e9ba3b2fda3ad46036b218c8fa6849
Mirwais Farahi <waisfarahi@gmail.com>
2022-06-11 17:10:31 +0500

    Edit utils.js File

--- src/utils.js
+++ src/utils.js
@@ -1,141 +1,111 @@
 import Todo from './todo.js';
+import LocalStorage from './storage.js';
+import Status from './status.js';
 
 class Operation {
-    static setData = (item) => localStorage.setItem('todoList', JSON.stringify(item));
+  // Add task function
+  static addTodo = (task) => {
+    if (task) {
+      // get data from localstorage
+      let todoList = LocalStorage.getData();
+      if (todoList == null) {
+        todoList = [];
+      }
 
-    static getData = () => JSON.parse(localStorage.getItem('todoList'));
+      // increment the value of index
+      const index = todoList.length + 1;
 
-    // reload the page
-    static reloadPage = () => {
-      window.location.reload();
-      return false;
+      // create an object of index, description and completed
+      const todo = new Todo(index, task);
+
+      // push newTask object to the array
+      todoList.push(todo);
+
+      // add array to the local storage
+      LocalStorage.setData(todoList);
+
+      // refresh the window
+      LocalStorage.reloadPage();
     }
+  }
 
-    // Add task function
-    static addTodo = (task) => {
-      if (task) {
-        // get data from localstorage
-        let todoList = this.getData();
-        if (todoList == null) {
-          todoList = [];
+  // renderTodo function
+  static renderTodo = () => {
+    // select todo list
+    const todoListItems = document.querySelector('#todo-list');
+
+    // clean up tolistItems
+    todoListItems.innerHTML = '';
+
+    const list = LocalStorage.getData();
+
+    // loop through the todo list
+    if (list) {
+      list.forEach((item) => {
+        // check if the task is completed
+        const checked = item.completed ? 'checked' : null;
+
+        // create an li element with class="item"
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item');
+
+        // if task completed then add checked class to the li to cross over the activity
+        if (checked === 'checked') {
+          li.classList.add('checked');
         }
 
-        // increment the value of index
-        const index = todoList.length + 1;
+        // add data, checkbox and delete button to li
+        li.innerHTML = `<input type = "checkbox" class="checkbox" ${checked}> ${item.description} <button class="edit"><i class="fa fa-edit" aria-hidden="true"></i></button> <button class="delete"><i class="fa fa-trash" aria-hidden="true"></i></button>`;
 
-        // create an object of index, description and completed
-        const todo = new Todo(index, task);
-
-        // push newTask object to the array
-        todoList.push(todo);
-
-        // add array to the local storage
-        this.setData(todoList);
-
-        // refresh the window
-        this.reloadPage();
-      }
-    }
-
-    // renderTodo function
-    static renderTodo = () => {
-      // select todo list
-      const todoListItems = document.querySelector('#todo-list');
-
-      // clean up tolistItems
-      todoListItems.innerHTML = '';
-
-      const list = this.getData();
-
-      // loop through the todo list
-      if (list) {
-        list.forEach((item) => {
-          // check if the task is completed
-          const checked = item.completed ? 'checked' : null;
-
-          // create an li element with class="item"
-          const li = document.createElement('li');
-          li.setAttribute('class', 'item');
-
-          // if task completed then add checked class to the li to cross over the activity
-          if (checked === 'checked') {
-            li.classList.add('checked');
-          }
-
-          // add data, checkbox and delete button to li
-          li.innerHTML = `<input type = "checkbox" class="checkbox" ${checked}> ${item.description} <button class="edit"><i class="fa fa-edit" aria-hidden="true"></i></button> <button class="delete"><i class="fa fa-trash" aria-hidden="true"></i></button>`;
-
-          todoListItems.appendChild(li);
-        });
-      }
-
-      // select delete from the list
-      const deleteTodo = document.querySelectorAll('.delete');
-      deleteTodo.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-          this.remove(index);
-        });
-      });
-
-      // select edit from the list
-      const edit = document.querySelectorAll('.edit');
-      edit.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-          this.edit(index);
-        });
-      });
-
-      const checkmarked = document.querySelectorAll('.checkbox');
-      checkmarked.forEach((btn, index) => {
-        btn.addEventListener('click', () => this.completedCheck(index));
+        todoListItems.appendChild(li);
       });
     }
 
-    // remove Todo
-    static remove = (index) => {
-      const todos = this.getData();
-      todos.splice(index, 1);
-
-      todos.forEach((todo, i) => {
-        todos[i].index = i + 1;
+    // select delete from the list
+    const deleteTodo = document.querySelectorAll('.delete');
+    deleteTodo.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        this.remove(index);
       });
+    });
 
-      this.setData(todos);
-      this.reloadPage();
-    }
-
-    // edit todo
-    static edit = (i) => {
-      const todos = this.getData();
-      const newDescription = prompt('Please Edit The Activity', todos[i].description);
-
-      // store the edit to local storage
-      if (newDescription) {
-        todos[i].description = newDescription;
-        this.setData(todos);
-      }
-
-      // refresh the page
-      this.reloadPage();
-    }
-
-    // check mark
-    static completedCheck = (i) => {
-      const todos = this.getData();
-      const todo = todos[i];
-
-      todos.forEach((item) => {
-        if (item.index === todo.index) {
-          if (item.completed === false) {
-            item.completed = true;
-          } else {
-            item.completed = false;
-          }
-        }
+    // select edit from the list
+    const edit = document.querySelectorAll('.edit');
+    edit.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        this.edit(index);
       });
-      this.setData(todos);
-      this.reloadPage();
+    });
+
+    const checkmarked = document.querySelectorAll('.checkbox');
+    checkmarked.forEach((btn, index) => {
+      btn.addEventListener('click', () => Status.completedCheck(index));
+    });
+  }
+
+  // edit todo
+  static edit = (i) => {
+    const todos = LocalStorage.getData();
+    const newDescription = prompt('Please Edit The Activity', todos[i].description);
+
+    // store the edit to local storage
+    if (newDescription) {
+      todos[i].description = newDescription;
+      LocalStorage.setData(todos);
     }
+
+    // refresh the page
+    LocalStorage.reloadPage();
+  }
+
+  // remove Todo
+  static remove = (index) => {
+    let todos = LocalStorage.getData();
+    todos.splice(index, 1);
+    todos = LocalStorage.updateIndex(todos);
+    LocalStorage.setData(todos);
+    LocalStorage.reloadPage();
+  }
 }
 
 export default Operation;
