3d36b06ef6ad2c316fc08c88957d294fce2c020a
Henry V. Singbeh <dokinruss@gmail.com>
2022-06-21 13:33:51 +0000

    Update utils.test.js

--- src/__test__/utils.test.js
+++ src/__test__/utils.test.js
@@ -6,4 +6,15 @@ import Operation from '../utils.js';
 
 jest.mock('../storage.js');
 
+describe('Test adTodo and remove Functions', () => {
+  test('test todo function', () => {
+    expect(Operation.addTodo('task 5')).toEqual([
+      { index: 1, description: 'task 1', completed: false },
+      { index: 2, description: 'task 2', completed: true },
+      { index: 3, description: 'task 3', completed: false },
+      { index: 4, description: 'task 4', completed: true },
+      { index: 5, description: 'task 5', completed: false },
+    ]);
+  });
+
 
